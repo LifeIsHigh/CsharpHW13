61df543db27dcba103063986a9438c9cd85c6eb4
Aldoshin_Alexandr <aldoshin_a@internet.ru>
2023-01-28 09:57:32 +0300

    Решение задачи №29 Семинар №4, Размер массива задаёт пользователь, значения в массиве генерятся случайным образом в диапазоне от -99 до 99

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача № 29 -->   Напишите программу, которая задаёт массив из 8 элементов
+//и выводит их на экран
+
+Console.Write("Enter the length of the array --> ");
+int Length = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[Length];
+
+void RandomArray(int[] array)
+{
+    for (int i = 0; i < Length; i++)
+    {
+        array[i] = new Random().Next(-99, 100);
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+RandomArray(array);
